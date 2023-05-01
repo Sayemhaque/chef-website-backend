0e7f1ebdb05647e7510e81bf43b71608c8abf310
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-02 00:45:59 +0600

    json data changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const PORT = 5000;
-
+const data = require("./data/data.json")
 const app = express()
 app.use(cors());
 
@@ -9,11 +9,17 @@ app.use(cors());
 
 
 app.get("/allchef" , (req,res) => {
-    res.send({data:"sayem"})
+    res.send(data)
 })
 
-
+app.get('/chef/:id' , (req,res) => {
+    const singleChef = data.find(single => single.id == req.params.id)
+    res.send(singleChef)
+})
 
 app.listen(PORT , () => {
     console.log(`server is running on http://localhost:${PORT}`)
 })
+
+
+//API URL : https://recipe-app-sayemhaque.vercel.app/allchef
