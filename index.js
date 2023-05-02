34a7aebed2b5359e9ce5463d31127226d7eddf97
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-02 12:00:34 +0600

    feature json data and route added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const PORT = 5000;
 const data = require("./data/data.json")
+const feature = require("./data/feature.json")
 const app = express()
 app.use(cors());
 
@@ -17,9 +18,16 @@ app.get('/chef/:id' , (req,res) => {
     res.send(singleChef)
 })
 
+
+app.get('/feature' , (req,res) => {
+    res.send(feature)
+})
+
 app.listen(PORT , () => {
     console.log(`server is running on http://localhost:${PORT}`)
 })
 
 
 //API URL : https://recipe-app-sayemhaque.vercel.app/allchef
+//SINGLE CHEF URL : https://recipe-app-sayemhaque.vercel.app/chef/:id
+//FETAURE URL : https://recipe-app-sayemhaque.vercel.app/fetaure
